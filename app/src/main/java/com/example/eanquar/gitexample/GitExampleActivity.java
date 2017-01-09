8a307ea68fa3804388540cb2f8544e045064911f
andreaq <q.andrea@hotmail.it>
2017-01-09 15:58:17 +0100

    My second commit

--- app/src/main/java/com/example/eanquar/gitexample/GitExampleActivity.java
+++ app/src/main/java/com/example/eanquar/gitexample/GitExampleActivity.java
@@ -10,5 +10,5 @@ public class GitExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_example);
     }
-    //exsddd
+    //exsdddd
 }
